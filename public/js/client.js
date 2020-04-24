a6ebe5b1dff2417c9bb7f88ef6c2222316353622
Olivia Bizimungu <47276353+oliviabiz@users.noreply.github.com>
2020-04-24 12:27:44 -0300

    changed origin url

--- public/js/client.js
+++ public/js/client.js
@@ -69,7 +69,7 @@ function initPlayer() {
         firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
     }
     player = new YT.Player('video', {
-        "origin" : "https://in-sync.azurewebsites.net/",
+        "origin" : "https://in-sync.azurewebsites.net",
         events: {
         'onReady':onPlayerReady,
         'onStateChange':onPlayerStateChange
